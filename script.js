b276e67df7514afc32289dcf217fe57098d98b3a
SoshuKo <chitian.kokkota@gmail.com>
2024-08-18 04:15:13 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
 const replacementRules = {
+    's': '$',
+    "p’m": "2",
     "p’": "P",
     "pm": "1",
     "bm": "B",
-    "p’m": "2",
     "hm": "M",
-    "t’": "T",
-    "tn": "3",
-    "dn": "D",
     "t’n": "4",
+    "tn": "3",
+    "t’": "T",
+    "dn": "D",
     "hn": "N",
     "hr": "R",
     "hl": "L",
     "dz": "Z",
     "c’": "C",
     "s’": "S",
+    "ch’": "6",
     "ch": "5",
     "j": "J",
-    "ch’": "6",
+    "sh’": "8",
     "sh": "7",
     "zh": "j",
-    "sh’": "8",
-    "k’": "K",
-    "kn": "9",
-    "gn": "G",
     "k’n": "#",
+    "kn": "9",
+    "k’": "K",
+    "gn": "G",
     "hng": "O",
     "ng": "o",
     "gh": "%"
@@ -33,9 +34,10 @@ function replaceText() {
     let inputText = document.getElementById("inputText").value;
     let replacedText = inputText;
 
-    for (let key in replacementRules) {
+    const sortedRules = Object.keys(replacementRules).sort((a, b) => b.length - a.length);
+    sortedRules.forEach(key => {
         replacedText = replacedText.split(key).join(replacementRules[key]);
-    }
+    });
 
     document.getElementById("output").innerText = replacedText;
 }
